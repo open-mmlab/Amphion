78a8cdcf5b85bd791334c14c2dd860aecda9513e
Xueyao Zhang <xueyao_98@foxmail.com>
2023-12-04 21:48:21 +0800

    Fix bugs for extracting whisper features (#7)
    * fix the bug of extracting whisper features and some typos

--- modules/whisper_extractor/__init__.py
+++ modules/whisper_extractor/__init__.py
@@ -36,7 +36,10 @@ from typing import List, Optional, Union
 import torch
 from tqdm import tqdm
 
+from .audio import load_audio, log_mel_spectrogram, pad_or_trim
+from .decoding import DecodingOptions, DecodingResult, decode, detect_language
 from .model import Whisper, ModelDimensions
+from .transcribe import transcribe
 from .version import __version__
 
 
