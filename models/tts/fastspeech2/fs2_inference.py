42ca016cbafc9737e63cc1cb2ecfa2582cbcadf3
ChenX17 <37540981+ChenX17@users.noreply.github.com>
2023-12-06 18:59:14 +0800

    Fix bugs for FS2 inference on updated phone_extractor (#12)
    * Fixed bugs for FS2 on updated phone_extractor
    
    * Update fs2_inference.py
    
    * Update fs2_inference.py
    
    * mv phone symbol to inference
    
    * rm { in text_token_collation.py
    
    ---------
    
    Co-authored-by: Liumeng Xue <33707885+lmxue@users.noreply.github.com>

--- models/tts/fastspeech2/fs2_inference.py
+++ models/tts/fastspeech2/fs2_inference.py
@@ -15,6 +15,10 @@ from utils.io import save_audio
 from models.tts.fastspeech2.fs2 import FastSpeech2
 from models.vocoders.vocoder_inference import synthesis
 from pathlib import Path
+from processors.phone_extractor import phoneExtractor
+from text.text_token_collation import phoneIDCollation
+import numpy as np
+import json
 
 
 class FastSpeech2Inference(TTSInference):
@@ -58,7 +62,7 @@ class FastSpeech2Inference(TTSInference):
         return vocoder_cfg, ckpt_path
 
     @torch.inference_mode()
-    def inference(self):
+    def inference_for_batches(self):
         y_pred = []
         for i, batch in tqdm(enumerate(self.test_dataloader)):
             y_pred, mel_lens, _ = self._inference_each_batch(batch)
@@ -117,3 +121,72 @@ class FastSpeech2Inference(TTSInference):
         pred_res = output["postnet_output"]
         mel_lens = output["mel_lens"].cpu()
         return pred_res, mel_lens, 0
+
+    def inference_for_single_utterance(self):
+        text = self.args.text
+        control_values = (
+            self.args.pitch_control,
+            self.args.energy_control,
+            self.args.duration_control,
+        )
+        pitch_control, energy_control, duration_control = control_values
+
+        # get phone symbol file
+        phone_symbol_file = None
+        if self.cfg.preprocess.phone_extractor != "lexicon":
+            phone_symbol_file = os.path.join(
+                self.exp_dir, self.cfg.preprocess.symbols_dict
+            )
+            assert os.path.exists(phone_symbol_file)
+        # convert text to phone sequence
+        phone_extractor = phoneExtractor(self.cfg)
+
+        phone_seq = phone_extractor.extract_phone(text)  # phone_seq: list
+        # convert phone sequence to phone id sequence
+        phon_id_collator = phoneIDCollation(
+            self.cfg, symbols_dict_file=phone_symbol_file
+        )
+        phone_seq = ["{"] + phone_seq + ["}"]
+        phone_id_seq = phon_id_collator.get_phone_id_sequence(self.cfg, phone_seq)
+
+        # convert phone sequence to phone id sequence
+        phone_id_seq = np.array(phone_id_seq)
+        phone_id_seq = torch.from_numpy(phone_id_seq)
+
+        # get speaker id if multi-speaker training and use speaker id
+        speaker_id = None
+        if self.cfg.preprocess.use_spkid and self.cfg.train.multi_speaker_training:
+            spk2id_file = os.path.join(self.exp_dir, self.cfg.preprocess.spk2id)
+            with open(spk2id_file, "r") as f:
+                spk2id = json.load(f)
+                speaker_id = spk2id[self.args.speaker_name]
+                speaker_id = torch.from_numpy(np.array([speaker_id], dtype=np.int32))
+        else:
+            speaker_id = torch.Tensor(0).view(-1)
+
+        with torch.no_grad():
+            x_tst = phone_id_seq.to(self.device).unsqueeze(0)
+            x_tst_lengths = torch.LongTensor([phone_id_seq.size(0)]).to(self.device)
+            if speaker_id is not None:
+                speaker_id = speaker_id.to(self.device)
+
+            data = {}
+            data["texts"] = x_tst
+            data["text_len"] = x_tst_lengths
+            data["spk_id"] = speaker_id
+
+            output = self.model(
+                data,
+                p_control=pitch_control,
+                e_control=energy_control,
+                d_control=duration_control,
+            )
+            pred_res = output["postnet_output"]
+            vocoder_cfg, vocoder_ckpt = self._parse_vocoder(self.args.vocoder_dir)
+            audio = synthesis(
+                cfg=vocoder_cfg,
+                vocoder_weight_file=vocoder_ckpt,
+                n_samples=None,
+                pred=pred_res,
+            )
+        return audio[0]
--- processors/phone_extractor.py
+++ processors/phone_extractor.py
@@ -8,135 +8,146 @@ from tqdm import tqdm
 from text.g2p_module import G2PModule, LexiconModule
 from text.symbol_table import SymbolTable
 
-'''
+"""
     phoneExtractor: extract phone from text
-'''
+"""
+
+
 class phoneExtractor:
     def __init__(self, cfg, dataset_name=None, phone_symbol_file=None):
-        '''
-            Args:
-                cfg: config
-                dataset_name: name of dataset
-        '''
+        """
+        Args:
+            cfg: config
+            dataset_name: name of dataset
+        """
         self.cfg = cfg
 
         #  phone symbols dict
         self.phone_symbols = set()
-        
+
         # phone symbols dict file
         if phone_symbol_file is not None:
             self.phone_symbols_file = phone_symbol_file
         elif dataset_name is not None:
             self.dataset_name = dataset_name
-            self.phone_symbols_file = os.path.join(cfg.preprocess.processed_dir, 
-                                            dataset_name, 
-                                            cfg.preprocess.symbols_dict)
+            self.phone_symbols_file = os.path.join(
+                cfg.preprocess.processed_dir, dataset_name, cfg.preprocess.symbols_dict
+            )
 
-        
         # initialize g2p module
-        if cfg.preprocess.phone_extractor in ["espeak", "pypinyin", "pypinyin_initials_finals"]:
+        if cfg.preprocess.phone_extractor in [
+            "espeak",
+            "pypinyin",
+            "pypinyin_initials_finals",
+        ]:
             self.g2p_module = G2PModule(backend=cfg.preprocess.phone_extractor)
-        elif cfg.preprocess.phone_extractor == 'lexicon':
+        elif cfg.preprocess.phone_extractor == "lexicon":
             assert cfg.preprocess.lexicon_path != ""
             self.g2p_module = LexiconModule(cfg.preprocess.lexicon_path)
         else:
-            print('No suppert to', cfg.preprocess.phone_extractor)
+            print("No suppert to", cfg.preprocess.phone_extractor)
             raise
 
-    
     def extract_phone(self, text):
-        '''
-            Extract phone from text
-            Args:
+        """
+        Extract phone from text
+        Args:
 
-                text:  text of utterance
-                
-            Returns:    
-                phone_symbols: set of phone symbols
-                phone_seq: list of phone sequence of each utterance
-        '''
-        
-        if self.cfg.preprocess.phone_extractor in ["espeak", "pypinyin", "pypinyin_initials_finals"]:
+            text:  text of utterance
+
+        Returns:
+            phone_symbols: set of phone symbols
+            phone_seq: list of phone sequence of each utterance
+        """
+
+        if self.cfg.preprocess.phone_extractor in [
+            "espeak",
+            "pypinyin",
+            "pypinyin_initials_finals",
+        ]:
             text = text.replace("”", '"').replace("“", '"')
-            phone = self.g2p_module.g2p_conversion(text=text)  
-            self.phone_symbols.update(phone)    
+            phone = self.g2p_module.g2p_conversion(text=text)
+            self.phone_symbols.update(phone)
             phone_seq = [phn for phn in phone]
-            
-        elif self.cfg.preprocess.phone_extractor == 'lexicon':
+
+        elif self.cfg.preprocess.phone_extractor == "lexicon":
             phone_seq = self.g2p_module.g2p_conversion(text)
             phone = phone_seq
             if not isinstance(phone_seq, list):
                 phone_seq = phone_seq.split()
-           
+
         return phone_seq
 
     def save_dataset_phone_symbols_to_table(self):
-        # load and merge saved phone symbols                
+        # load and merge saved phone symbols
         if os.path.exists(self.phone_symbols_file):
-            phone_symbol_dict_saved = SymbolTable.from_file(self.phone_symbols_file)._sym2id.keys()
+            phone_symbol_dict_saved = SymbolTable.from_file(
+                self.phone_symbols_file
+            )._sym2id.keys()
             self.phone_symbols.update(set(phone_symbol_dict_saved))
 
         # save phone symbols
         phone_symbol_dict = SymbolTable()
         for s in sorted(list(self.phone_symbols)):
             phone_symbol_dict.add(s)
-        phone_symbol_dict.to_file(self.phone_symbols_file)    
+        phone_symbol_dict.to_file(self.phone_symbols_file)
 
-                
+
 def extract_utt_phone_sequence(cfg, metadata):
-    '''
-        Extract phone sequence from text
-        Args:
-            cfg: config
-            metadata: list of dict, each dict contains "Uid", "Text"
-            
-    '''
-    
+    """
+    Extract phone sequence from text
+    Args:
+        cfg: config
+        metadata: list of dict, each dict contains "Uid", "Text"
+
+    """
+
     dataset_name = cfg.dataset[0]
-    
+
     # output path
-    out_path = os.path.join(cfg.preprocess.processed_dir, dataset_name, cfg.preprocess.phone_dir)
+    out_path = os.path.join(
+        cfg.preprocess.processed_dir, dataset_name, cfg.preprocess.phone_dir
+    )
     os.makedirs(out_path, exist_ok=True)
-        
+
     phone_extractor = phoneExtractor(cfg, dataset_name)
 
-    for utt in tqdm(metadata):  
+    for utt in tqdm(metadata):
         uid = utt["Uid"]
-        text = utt["Text"]    
-                 
+        text = utt["Text"]
+
         phone_seq = phone_extractor.extract_phone(text)
-                 
-        phone_path = os.path.join(out_path, uid+'.phone')
-        with open(phone_path, 'w') as fin:
-            fin.write(' '.join(phone_seq))
-    
-    if cfg.preprocess.phone_extractor != 'lexicon':
+
+        phone_path = os.path.join(out_path, uid + ".phone")
+        with open(phone_path, "w") as fin:
+            fin.write(" ".join(phone_seq))
+
+    if cfg.preprocess.phone_extractor != "lexicon":
         phone_extractor.save_dataset_phone_symbols_to_table()
-    
-    
-        
+
+
 def save_all_dataset_phone_symbols_to_table(self, cfg, dataset):
     #  phone symbols dict
     phone_symbols = set()
-    
+
     for dataset_name in dataset:
-        phone_symbols_file = os.path.join(cfg.preprocess.processed_dir, 
-                                          dataset_name, 
-                                          cfg.preprocess.symbols_dict)
-        
-        # load and merge saved phone symbols                
+        phone_symbols_file = os.path.join(
+            cfg.preprocess.processed_dir, dataset_name, cfg.preprocess.symbols_dict
+        )
+
+        # load and merge saved phone symbols
         assert os.path.exists(phone_symbols_file)
-        phone_symbol_dict_saved = SymbolTable.from_file(phone_symbols_file)._sym2id.keys()
+        phone_symbol_dict_saved = SymbolTable.from_file(
+            phone_symbols_file
+        )._sym2id.keys()
         phone_symbols.update(set(phone_symbol_dict_saved))
-        
+
     # save all phone symbols to each dataset
     phone_symbol_dict = SymbolTable()
     for s in sorted(list(phone_symbols)):
         phone_symbol_dict.add(s)
     for dataset_name in dataset:
-        phone_symbols_file = os.path.join(cfg.preprocess.processed_dir, 
-                                          dataset_name, 
-                                          cfg.preprocess.symbols_dict)
-        phone_symbol_dict.to_file(phone_symbols_file)    
-        
-          
+        phone_symbols_file = os.path.join(
+            cfg.preprocess.processed_dir, dataset_name, cfg.preprocess.symbols_dict
+        )
+        phone_symbol_dict.to_file(phone_symbols_file)
