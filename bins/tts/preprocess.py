2af9b4d71a11110dd354a6c72dcec1c3f0701d5f
lmxue <xueliumeng@gmail.com>
2023-12-13 03:10:13 +0800

    make metadata.py more flexible to different json files

--- bins/tts/preprocess.py
+++ bins/tts/preprocess.py
@@ -19,7 +19,7 @@ from preprocessors.metadata import cal_metadata
 from processors import acoustic_extractor, content_extractor, data_augment, phone_extractor
 
 
-def extract_acoustic_features(dataset, output_path, cfg, n_workers=1):
+def extract_acoustic_features(dataset, output_path, cfg, dataset_types, n_workers=1):
     """Extract acoustic features of utterances in the dataset
 
     Args:
@@ -28,17 +28,9 @@ def extract_acoustic_features(dataset, output_path, cfg, n_workers=1):
         cfg (dict): dictionary that stores configurations
         n_workers (int, optional): num of processes to extract features in parallel. Defaults to 1.
     """
-    # types = ["train", "test"] if "eval" not in dataset else ["test"]
-    types = list()
-    types.append((cfg.preprocess.train_file).split('.')[0])
-    types.append((cfg.preprocess.valid_file).split('.')[0])
-    if 'test' not in types: 
-        types.append('test') 
-    if "eval" in dataset:
-        types = ["test"]
-    print('types: ', types)
+
     metadata = []
-    for dataset_type in types:
+    for dataset_type in dataset_types:
         dataset_output = os.path.join(output_path, dataset)
         dataset_file = os.path.join(dataset_output, "{}.json".format(dataset_type))
         with open(dataset_file, "r") as f:
@@ -52,7 +44,7 @@ def extract_acoustic_features(dataset, output_path, cfg, n_workers=1):
     )
 
 
-def extract_content_features(dataset, output_path, cfg, num_workers=1):
+def extract_content_features(dataset, output_path, cfg, dataset_types, num_workers=1):
     """Extract content features of utterances in the dataset
 
     Args:
@@ -60,18 +52,9 @@ def extract_content_features(dataset, output_path, cfg, num_workers=1):
         output_path (str): directory that stores train, test and feature files of datasets
         cfg (dict): dictionary that stores configurations
     """
-    # types = ["train", "test"] if "eval" not in dataset else ["test"]
-
-    types = list()
-    types.append((cfg.preprocess.train_file).split('.')[0])
-    types.append((cfg.preprocess.valid_file).split('.')[0])
-    if 'test' not in types: 
-        types.append('test') 
-    if "eval" in dataset:
-        types = ["test"]
             
     metadata = []
-    for dataset_type in types:
+    for dataset_type in dataset_types:
         dataset_output = os.path.join(output_path, dataset)
         # dataset_file = os.path.join(dataset_output, "{}.json".format(dataset_type))
         dataset_file = os.path.join(dataset_output, "{}.json".format(dataset_type))
@@ -82,7 +65,7 @@ def extract_content_features(dataset, output_path, cfg, num_workers=1):
         cfg, metadata, num_workers
     )
 
-def extract_phonme_sequences(dataset, output_path, cfg):
+def extract_phonme_sequences(dataset, output_path, cfg, dataset_types):
     """Extract phoneme features of utterances in the dataset
 
     Args:
@@ -91,18 +74,9 @@ def extract_phonme_sequences(dataset, output_path, cfg):
         cfg (dict): dictionary that stores configurations
 
     """
-    # types = ["train", "test"] if "eval" not in dataset else ["test"]
-
-    types = list()
-    types.append((cfg.preprocess.train_file).split('.')[0])
-    types.append((cfg.preprocess.valid_file).split('.')[0])
-    if 'test' not in types: 
-        types.append('test') 
-    if "eval" in dataset:
-        types = ["test"]
             
     metadata = []
-    for dataset_type in types:
+    for dataset_type in dataset_types:
         dataset_output = os.path.join(output_path, dataset)
         dataset_file = os.path.join(dataset_output, "{}.json".format(dataset_type))
         with open(dataset_file, "r") as f:
@@ -157,8 +131,17 @@ def preprocess(cfg, args):
     except:
         print("No Data Augmentation.")
 
+    # json files
+    dataset_types = list()
+    dataset_types.append((cfg.preprocess.train_file).split('.')[0])
+    dataset_types.append((cfg.preprocess.valid_file).split('.')[0])
+    if 'test' not in dataset_types: 
+        dataset_types.append('test') 
+    if "eval" in dataset:
+        dataset_types = ["test"]
+        
     # Dump metadata of datasets (singers, train/test durations, etc.)
-    cal_metadata(cfg)
+    cal_metadata(cfg, dataset_types)
 
     ## Prepare the acoustic features
     for dataset in cfg.dataset:
@@ -175,7 +158,7 @@ def preprocess(cfg, args):
                 dataset, args.num_workers
             )
         )
-        extract_acoustic_features(dataset, output_path, cfg, args.num_workers)
+        extract_acoustic_features(dataset, output_path, cfg, dataset_types, args.num_workers)
         # Calculate the statistics of acoustic features
         if cfg.preprocess.mel_min_max_norm:
             acoustic_extractor.cal_mel_min_max(dataset, output_path, cfg)
@@ -224,13 +207,13 @@ def preprocess(cfg, args):
     # Prepare the content features
     for dataset in cfg.dataset:
         print("Extracting content features for {}...".format(dataset))
-        extract_content_features(dataset, output_path, cfg, args.num_workers)
+        extract_content_features(dataset, output_path, cfg, dataset_types, args.num_workers)
 
     # Prepare the phenome squences
     if cfg.preprocess.extract_phone:
         for dataset in cfg.dataset:
             print("Extracting phoneme sequence for {}...".format(dataset))
-            extract_phonme_sequences(dataset, output_path, cfg)
+            extract_phonme_sequences(dataset, output_path, cfg, dataset_types)
             
 def main():
     parser = argparse.ArgumentParser()
--- preprocessors/metadata.py
+++ preprocessors/metadata.py
@@ -8,7 +8,7 @@ import json
 from tqdm import tqdm
 
 
-def cal_metadata(cfg):
+def cal_metadata(cfg, dataset_types):
     """
     Dump metadata (singers.json, meta_info.json, utt2singer) for singer dataset or multi-datasets.
     """
@@ -25,52 +25,48 @@ def cal_metadata(cfg):
     for dataset in tqdm(datasets):
         save_dir = os.path.join(cfg.preprocess.processed_dir, dataset)
         assert os.path.exists(save_dir)
-
+        
         # 'train.json' and 'test.json' and 'valid.json' of target dataset
-        train_metadata = os.path.join(save_dir, "train.json")
-        test_metadata = os.path.join(save_dir, "test.json")
-        valid_metadata = os.path.join(save_dir, "valid.json")
+        meta_info = dict()
+        utterances_dict = dict()
+        all_utterances = list()
+        duration = dict()
+        total_duration = 0.0
+        for data_type in dataset_types:
+            metadata = os.path.join(save_dir, "{}.json".format(data_type))
+            
+            # Sort the metadata as the duration order
+            with open(metadata, "r", encoding="utf-8") as f:
+                utterances = json.load(f)      
+            utterances = sorted(utterances, key=lambda x: x["Duration"])   
+            utterances_dict[data_type] = utterances
+            all_utterances.extend(utterances)
 
-        # Sort the metadata as the duration order
-        with open(train_metadata, "r", encoding="utf-8") as f:
-            train_utterances = json.load(f)
-        with open(test_metadata, "r", encoding="utf-8") as f:
-            test_utterances = json.load(f)
-        with open(valid_metadata, "r", encoding="utf-8") as f:
-            valid_utterances = json.load(f)
+            # Write back the sorted metadata
+            with open(metadata, "w") as f:
+                json.dump(utterances, f, indent=4, ensure_ascii=False)
 
-        train_utterances = sorted(train_utterances, key=lambda x: x["Duration"])
-        test_utterances = sorted(test_utterances, key=lambda x: x["Duration"])
-        valid_utterances = sorted(valid_utterances, key=lambda x: x["Duration"])
+            # Get the total duration and singer names for train and test utterances
+            duration[data_type] = sum(utt["Duration"] for utt in utterances)
+            total_duration += duration[data_type]
 
-        # Write back the sorted metadata
-        with open(train_metadata, "w") as f:
-            json.dump(train_utterances, f, indent=4, ensure_ascii=False)
-        with open(test_metadata, "w") as f:
-            json.dump(test_utterances, f, indent=4, ensure_ascii=False)
-        with open(valid_metadata, "w") as f:
-            json.dump(valid_utterances, f, indent=4, ensure_ascii=False)
+        
 
         # Paths of metadata needed to be generated
         singer_dict_file = os.path.join(save_dir, cfg.preprocess.spk2id)
         utt2singer_file = os.path.join(save_dir, cfg.preprocess.utt2spk)
 
-        # Get the total duration and singer names for train and test utterances
-        train_total_duration = sum(utt["Duration"] for utt in train_utterances)
-        test_total_duration = sum(utt["Duration"] for utt in test_utterances)
-        valid_total_duration = sum(utt["Duration"] for utt in valid_utterances)
-
         singer_names = set(
             f"{replace_augment_name(utt['Dataset'])}_{utt['Singer']}"
-            for utt in train_utterances + test_utterances + valid_utterances
+            for utt in all_utterances
         )
 
         # Write the utt2singer file and sort the singer names
         with open(utt2singer_file, "w", encoding="utf-8") as f:
-            for utt in train_utterances + test_utterances + valid_utterances:
+            for utt in all_utterances:
                 f.write(
                     f"{utt['Dataset']}_{utt['Uid']}\t{replace_augment_name(utt['Dataset'])}_{utt['Singer']}\n"
-                )
+                )            
 
         singer_names = sorted(singer_names)
         singer_lut = {name: i for i, name in enumerate(singer_names)}
@@ -82,39 +78,29 @@ def cal_metadata(cfg):
         meta_info = {
             "dataset": dataset,
             "statistics": {
-                "size": len(train_utterances) + len(test_utterances) + len(valid_utterances),
-                "hours": round(train_total_duration / 3600, 4)
-                + round(test_total_duration / 3600, 4)
-                + round(valid_total_duration / 3600, 4),
-            },
-            "train": {
-                "size": len(train_utterances),
-                "hours": round(train_total_duration / 3600, 4),
-            },
-            "test": {
-                "size": len(test_utterances),
-                "hours": round(test_total_duration / 3600, 4),
-            },
-            "valid": {
-                "size": len(valid_utterances),
-                "hours": round(valid_total_duration / 3600, 4),
-            },
-            "singers": {"size": len(singer_lut)},
+                "size": len(all_utterances),
+                "hours": round(total_duration / 3600, 4)
+            }
         }
+        
+        for data_type in dataset_types:
+            meta_info[data_type] = {
+                    "size": len(utterances_dict[data_type]),
+                    "hours": round(duration[data_type] / 3600, 4),
+                }
+        
+        meta_info["singers"] = {"size": len(singer_lut)}
+                        
         # Use Counter to count the minutes for each singer
         total_singer2mins = Counter()
         training_singer2mins = Counter()
-        for utt in train_utterances:
-            k = f"{replace_augment_name(utt['Dataset'])}_{utt['Singer']}"
-            training_singer2mins[k] += utt["Duration"] / 60
-            total_singer2mins[k] += utt["Duration"] / 60
-        for utt in test_utterances:
-            k = f"{replace_augment_name(utt['Dataset'])}_{utt['Singer']}"
-            total_singer2mins[k] += utt["Duration"] / 60
-        for utt in valid_utterances:
-            k = f"{replace_augment_name(utt['Dataset'])}_{utt['Singer']}"
-            total_singer2mins[k] += utt["Duration"] / 60
-
+        for data_type in dataset_types:
+            for utt in utterances_dict[data_type]:
+                k = f"{replace_augment_name(utt['Dataset'])}_{utt['Singer']}"
+                if data_type == 'train':
+                    training_singer2mins[k] += utt["Duration"] / 60
+                total_singer2mins[k] += utt["Duration"] / 60
+                        
         training_singer2mins = dict(
             sorted(training_singer2mins.items(), key=lambda x: x[1], reverse=True)
         )
@@ -131,8 +117,9 @@ def cal_metadata(cfg):
             json.dump(meta_info, f, indent=4, ensure_ascii=False)
 
         for singer, min in training_singer2mins.items():
-            print(f"Singer {singer}: {min} mins for training")
+            print(f"Speaker/Singer {singer}: {min} mins for training")
         print("-" * 10, "\n")
+
 
 
 def replace_augment_name(dataset: str) -> str:
