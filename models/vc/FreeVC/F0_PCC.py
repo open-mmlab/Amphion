8c5e3413ec162b6307e3fe2eec46c771b664fbf7
Nugine <nugine@foxmail.com>
2024-05-08 12:06:03 +0800

    format

--- models/vc/FreeVC/F0_PCC.py
+++ models/vc/FreeVC/F0_PCC.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import numpy as np
 import pyworld as pw
 import argparse
@@ -9,34 +8,52 @@ import os
 def get_f0(x, fs=16000, n_shift=160):
     x = x.astype(np.float64)
     frame_period = n_shift / fs * 1000
-    f0, timeaxis = pw.dio(x, fs, frame_period=frame_period)
-    f0 = pw.stonemask(x, f0, timeaxis, fs)
+    f0, timeaxis = pw.dio(x, fs, frame_period=frame_period)  # type:ignore
+    f0 = pw.stonemask(x, f0, timeaxis, fs)  # type:ignore
     return f0
-    
-    
+
+
 def compute_f0(wav, sr=16000, frame_period=10.0):
     wav = wav.astype(np.float64)
-    f0, timeaxis = pw.harvest(
-        wav, sr, frame_period=frame_period, f0_floor=20.0, f0_ceil=600.0)
+    f0, timeaxis = pw.harvest(  # type:ignore
+        wav, sr, frame_period=frame_period, f0_floor=20.0, f0_ceil=600.0
+    )
     return f0
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("--txtpath", type=str, default="samples.txt", help="path to txt file")
-    parser.add_argument("--src_path", type=str, default=r"data\VCTK\test_data", help="path to src audio files")
-    parser.add_argument("--tgt_path", type=str, default=r"data\VCTK\test_output", help="path to output audio files")
-    parser.add_argument("--output_path", type=str, default=r"result\pcc.txt", help="path tot the pcc output file")
+    parser.add_argument(
+        "--src_path",
+        type=str,
+        default=r"data\VCTK\test_data",
+        help="path to src audio files",
+    )
+    parser.add_argument(
+        "--tgt_path",
+        type=str,
+        default=r"data\VCTK\test_output",
+        help="path to output audio files",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default=r"result\pcc.txt",
+        help="path tot the pcc output file",
+    )
     args = parser.parse_args()
-    
+
     pccs = []
     i = 0
-    src_files = [f for f in os.listdir(args.src_path) if f.endswith('.wav')]
+    src_files = [f for f in os.listdir(args.src_path) if f.endswith(".wav")]
 
     for filename in src_files:
         # print(filename)
         path_of_src_audio = os.path.join(args.src_path, filename)
-        path_of_res_audio = os.path.join(args.tgt_path, os.path.splitext(filename)[0]+"_sync.wav")
+        path_of_res_audio = os.path.join(
+            args.tgt_path, os.path.splitext(filename)[0] + "_sync.wav"
+        )
         # print(os.path.exists(path_of_res_audio))
 
         if os.path.exists(path_of_res_audio):
@@ -50,15 +67,17 @@ if __name__ == "__main__":
                 src_f0 = compute_f0(src)
                 tgt_f0 = compute_f0(tgt)
                 # print(rawline)
-            pcc = np.corrcoef(src_f0[:tgt_f0.shape[-1]], tgt_f0[:src_f0.shape[-1]])[0, 1]
+            pcc = np.corrcoef(src_f0[: tgt_f0.shape[-1]], tgt_f0[: src_f0.shape[-1]])[
+                0, 1
+            ]
             print(pcc)
-            #print(i, pcc)
+            # print(i, pcc)
             if not np.isnan(pcc.item()):
                 pccs.append(pcc.item())
 
         else:
             print(f"Warning: No matching file for {filename} in result folder")
-    
+
     with open(args.output_path, "w") as f:
         for pcc in pccs:
             f.write(f"{pcc}\n")
--- models/vc/FreeVC/get_gt.py
+++ models/vc/FreeVC/get_gt.py
@@ -1,5 +1,4 @@
 from transformers import Wav2Vec2Processor, HubertForCTC
-import os
 import argparse
 import torch
 import librosa
@@ -8,22 +7,28 @@ from glob import glob
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--txtpath", type=str, default="gt.txt", help="path to tgt txt file")
+    parser.add_argument(
+        "--txtpath", type=str, default="gt.txt", help="path to tgt txt file"
+    )
     parser.add_argument("--wavdir", type=str, default=r"data\VCTK\test_data")
     args = parser.parse_args()
 
     # load model and processor
-    model_text = HubertForCTC.from_pretrained("facebook/hubert-large-ls960-ft").cuda()
+    model_text = HubertForCTC.from_pretrained(
+        "facebook/hubert-large-ls960-ft"
+    ).cuda()  # type:ignore
     processor_text = Wav2Vec2Processor.from_pretrained("facebook/hubert-large-ls960-ft")
-       
+
     # get transcriptions
-    wavs = glob(f'{args.wavdir}/*.wav')
+    wavs = glob(f"{args.wavdir}/*.wav")
     wavs.sort()
     with open(f"{args.txtpath}", "w") as f:
         for path in tqdm(wavs):
             wav = [librosa.load(path, sr=16000)[0]]
-            input_values = processor_text(wav, return_tensors="pt").input_values.cuda() # text
+            input_values = processor_text(
+                wav, return_tensors="pt"
+            ).input_values.cuda()  # text # type:ignore
             logits = model_text(input_values).logits
             predicted_ids = torch.argmax(logits, dim=-1)
-            text = processor_text.batch_decode(predicted_ids)[0]
+            text = processor_text.batch_decode(predicted_ids)[0]  # type:ignore
             f.write(f"{path}|{text}\n")
