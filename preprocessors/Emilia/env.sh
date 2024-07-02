3b1f1fdf0fdfb421bfa865b38ec4c32864d57dac
Harry He <68176557+HarryHe11@users.noreply.github.com>
2024-07-02 14:38:09 +0800

    Update env.sh

--- preprocessors/Emilia/env.sh
+++ preprocessors/Emilia/env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) 2024 Amphion.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 conda install ffmpeg -y
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
