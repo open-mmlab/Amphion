e66f8407993b8fbab59dfe196dfc76a067c8f4c6
hansheng-zhang <1718810184@qq.com>
2024-07-13 16:03:37 +0800

    add acknowledgement for jets.py

--- models/tts/jets/jets.py
+++ models/tts/jets/jets.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2023 Amphion.
-#
+# Copyright 2022 Dan Lim
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
