8321790cce178137d1d562ddf0fdf01f06aff9dd
felix_red_panda <felix_red_panda@protonmail.com>
2024-11-04 17:45:51 +0100

    duplicate code removal

--- models/tts/maskgct/maskgct_s2a.py
+++ models/tts/maskgct/maskgct_s2a.py
@@ -32,27 +32,6 @@ def gumbel_sample(t, temperature=1.0, dim=-1):
     return ((t / max(temperature, 1e-10)) + gumbel_noise(t)).argmax(dim=dim)
 
 
-def top_k(logits, thres=0.9):
-    k = math.ceil((1 - thres) * logits.shape[-1])
-    val, ind = logits.topk(k, dim=-1)
-    probs = torch.full_like(logits, float("-inf"))
-    probs.scatter_(2, ind, val)
-    return probs
-
-
-def log(t, eps=1e-10):
-    return torch.log(t + eps)
-
-
-def gumbel_noise(t):
-    noise = torch.zeros_like(t).uniform_(0, 1)
-    return -log(-log(noise))
-
-
-def gumbel_sample(t, temperature=1.0, dim=-1):
-    return ((t / max(temperature, 1e-10)) + gumbel_noise(t)).argmax(dim=dim)
-
-
 class MaskGCT_S2A(nn.Module):
     def __init__(
         self,
